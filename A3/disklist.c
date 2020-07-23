19dd99a8cd5639cd39a168cb37421c107a0ac31b
Cobey Hollier <cobey.hollier@referralsaasquatch.com>
2020-07-23 00:21:04 -0700

    CH: Adding functionality for part 2

--- A3/disklist.c
+++ A3/disklist.c
@@ -3,25 +3,77 @@
 #include <netinet/in.h>
 #include "Constants.h"
 
+struct Date {
+    int16_t year;
+    int8_t month;
+    int8_t day;
+    int8_t hours;
+    int8_t minutes;
+    int8_t seconds;
+};
+
 struct RootBlock {
-    unsigned char status;
-    int32_t startBlock;
-    int32_t numBlocks;
-    int32_t fileSize;
-    unsigned char createTime[7];
-    unsigned char modifyTime[7];
-    unsigned char fileName[31];
+    int8_t status;
+    int startBlock;
+    int numBlocks;
+    int fileSize;
+    struct Date createTime;
+    struct Date modifyTime;
+    char fileName[31];
 };
 
 struct SuperBlock {
-    char    ident[8];
+    char  ident[8];
     int16_t blockSize;
-    int32_t fileSystemSize;
-    int32_t fatStart;
-    int32_t blocksInFat;
-    int32_t rootStart;
-    int32_t blocksInRoot;
+    int fileSystemSize;
+    int fatStart;
+    int blocksInFat;
+    int rootStart;
+    int blocksInRoot;
 };
+
+int readInt32(FILE * inputFile) {
+    int32_t buffer;
+    fread(&buffer, 1, sizeof(buffer), inputFile);
+    return ntohl(buffer);
+}
+
+int readInt16(FILE * inputFile) {
+    int16_t buffer;
+    fread(&buffer, 1, sizeof(buffer), inputFile);
+    return ntohs(buffer);
+}
+
+void getDate(FILE *inputFile, struct Date *date) {
+    date->year = readInt16(inputFile);
+    fread(&date->month, 1, 5, inputFile);
+}
+
+void getNextRootBlock(FILE *inputFile, struct RootBlock *block) {
+    fread(&block->status, 1, 1, inputFile);
+    block->startBlock = readInt32(inputFile);
+    block->numBlocks  = readInt32(inputFile);
+    block->fileSize   = readInt32(inputFile);
+    getDate(inputFile, &block->createTime);
+    getDate(inputFile, &block->modifyTime);
+    fread(&block->fileName, 1, DIRECTORY_MAX_NAME_LENGTH, inputFile);
+}
+
+void printDate(struct Date date) {
+    printf("%d/%d/%d %d:%d:%d\n", date.year, date.month, date.day, date.hours, date.minutes, date.seconds);
+}
+
+void printBlock(struct RootBlock block) {
+    int isFile = block.status & DIRECTORY_ENTRY_FILE;
+    int isDirectory = block.status & DIRECTORY_ENTRY_DIRECTORY;
+    if(isFile) {
+        printf("F ");
+    } else if(isDirectory) {
+        printf("D ");
+    }
+    printf("%10d %30s ", block.fileSize, block.fileName);
+    printDate(block.modifyTime);
+}
 
 int main(int argc, char *argv[]) {
     if( argc < 2  ) {
@@ -30,26 +82,22 @@ int main(int argc, char *argv[]) {
     } 
     char *filename = argv[1];
     FILE *inputFile = fopen(filename, "r");
-    struct SuperBlock block;
+    struct SuperBlock sBlock;
     if(inputFile == NULL) {
         printf("Failed to open input file");
         exit(0);
     }
     // The data is all read into the struct so that individual sections can be accessed
-    fread(&block, 1, 30, inputFile);
+    fread(&sBlock, 1, 30, inputFile);
     // Seek to the start of the root directory
-    fseek(inputFile, ntohs(block.rootStart) * DEFAULT_BLOCK_SIZE, SEEK_SET);
-    printf("Start: %4X \n", ntohs(block.rootStart) * DEFAULT_BLOCK_SIZE);
-    struct RootBlock file;
-    fread(&file, 1, DIRECTORY_ENTRY_SIZE, inputFile);
-    printf("Status: %2X \n", file.status);
-    printf("Start Block: %4X \n", file.startBlock);
-    printf("Num Blocks: %4X \n", file.numBlocks); 
-    printf("fileSize: %4X \n", ntohs(file.fileSize));
-    for(int i = 0; i < 31; i++) {
-        printf("%c", file.fileName[i]);
+    int startPoint = ntohs(sBlock.rootStart) * DEFAULT_BLOCK_SIZE;
+    fseek(inputFile, startPoint, SEEK_SET);
+    for(int i = 1; i <= ntohs(sBlock.blocksInRoot); i++) {
+        struct RootBlock currRootBlock;
+        getNextRootBlock(inputFile, &currRootBlock);
+        printBlock(currRootBlock);
+        fseek(inputFile, startPoint + i * DIRECTORY_ENTRY_SIZE, SEEK_SET);
     }
-    printf("\n");
 
     fclose(inputFile);
     return 1;
